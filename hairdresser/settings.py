8d040cb0193cec60ab4d3bc9fed31fb8b95d1bb6
Maxim <nezarikintmax@gmail.com>
2025-03-29 13:34:49 +0500

    SSH test

--- hairdresser/settings.py
+++ hairdresser/settings.py
@@ -143,7 +143,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10 
 }
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
