e4b445661b67f76a247e99c7067252af31a1eeda
Maxim <nezarikintmax@gmail.com>
2025-04-06 18:46:22 +0500

    Новый рут гита2

--- schedule/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Client, Master, Service, Appointment
-
-admin.site.register(Client)
-admin.site.register(Master)
-admin.site.register(Service)
-admin.site.register(Appointment)
